b5eb2e7a1201a2a8affd2896966f8f33c43ce475
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-10-15 13:18:38 +0530

    SECOND REACT NATIVE APP

---
+++ sapp/App.tsx
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import {
+  Button,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = () =>{
+  return(
+    <View>
+      <Text  style = {{fontSize:30}}>WELCOME TO THE WORLD OF REACT NATIVE APPLICATION</Text>
+      <Button title='THIS IS MY FIRST BUTTON IN REACT NATIVE'></Button>
+      <Text style = {{fontSize:30}}> Hello SECOND TEXT Components</Text>
+      <Button title='THIS IS MY SECOND BUTTON IN REACT NATIVE'></Button>
+      <Text style = {{fontSize:30}}> Hello THIRD Text Components</Text>
+
+      <Text style = {{fontSize:30}}> Hello SECOND TEXT Components</Text>
+      <Button title='THIS IS MY SECOND BUTTON IN REACT NATIVE'></Button>
+      <Text style = {{fontSize:30}}> Hello THIRD Text Components</Text>
+
+      <Text style = {{fontSize:30}}> Hello SECOND TEXT Components</Text>
+      <Button title='THIS IS MY SECOND BUTTON IN REACT NATIVE'></Button>
+      <Text style = {{fontSize:30}}> Hello THIRD Text Components</Text>
+      <Button title='PRESS ME'></Button>
+
+    
+    </View>
+  )
+}
+export default App;
