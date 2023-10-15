6710ead7b2043b3c814dac2f1ece1548e37b948e
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-10-15 14:20:45 +0530

    foursth app

--- thirdapp/App.tsx
+++ thirdapp/App.tsx
@@ -17,6 +17,29 @@ const App = () =>{
       <Button title='On Press' onPress={() => fruit("Hello ")} color={'green'}/>
       <Button title='On Press' onPress={() => fruit("Hii")} color={'pink'}/>
 
+      <Text style = {{fontSize:30}}> Button and onPress Event</Text>
+      <Button title='On Press' onPress={() => fruit("Hello ")} color={'green'}/>
+      <Button title='On Press' onPress={() => fruit("Hii")} color={'pink'}/>
+
+      <Text style = {{fontSize:30}}> Button and onPress Event</Text>
+      <Button title='On Press' onPress={() => fruit("Hello ")} color={'green'}/>
+      <Button title='On Press' onPress={() => fruit("Hii")} color={'pink'}/>
+
+      <Text style = {{fontSize:30}}> Button and onPress Event</Text>
+      <Button title='On Press' onPress={() => fruit("Hello ")} color={'green'}/>
+      <Button title='On Press' onPress={() => fruit("Hii")} color={'pink'}/>
+
+      <Text style = {{fontSize:30}}> Button and onPress Event</Text>
+      <Button title='On Press' onPress={() => fruit("Hello ")} color={'green'}/>
+      <Button title='On Press' onPress={() => fruit("Hii")} color={'pink'}/>
+
+      <Text style = {{fontSize:30}}> Button and onPress Event</Text>
+      <Button title='On Press' onPress={() => fruit("Hello ")} color={'green'}/>
+      <Button title='On Press' onPress={() => fruit("Hii")} color={'pink'}/>
+
+      <Text style = {{fontSize:30}}> Button and onPress Event</Text>
+      <Button title='On Press' onPress={() => fruit("Hello ")} color={'green'}/>
+      <Button title='Final Press Submit ' onPress={() => fruit("Hii")}/>
     </View>
   )
 }
