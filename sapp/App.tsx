58d4ac9915018ecafcbad83b10cd1cbf7982d831
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-10-15 13:41:04 +0530

    second app

--- sapp/App.tsx
+++ sapp/App.tsx
@@ -6,26 +6,46 @@ import {
   View,
 } from 'react-native';
 
-
+const name = "Sagar";
+let age = 39;
+var email = 'spgiradkar2002@gmail.com';
+function fruit(){
+  return "Apple";
+}
 const App = () =>{
   return(
     <View>
-      <Text  style = {{fontSize:30}}>WELCOME TO THE WORLD OF REACT NATIVE APPLICATION</Text>
-      <Button title='THIS IS MY FIRST BUTTON IN REACT NATIVE'></Button>
-      <Text style = {{fontSize:30}}> Hello SECOND TEXT Components</Text>
-      <Button title='THIS IS MY SECOND BUTTON IN REACT NATIVE'></Button>
-      <Text style = {{fontSize:30}}> Hello THIRD Text Components</Text>
+      <Text  style = {{fontSize:30}}>Name : {name}</Text>
 
-      <Text style = {{fontSize:30}}> Hello SECOND TEXT Components</Text>
-      <Button title='THIS IS MY SECOND BUTTON IN REACT NATIVE'></Button>
-      <Text style = {{fontSize:30}}> Hello THIRD Text Components</Text>
+      <Text  style = {{fontSize:30}}>Age : {age}</Text>
 
-      <Text style = {{fontSize:30}}> Hello SECOND TEXT Components</Text>
-      <Button title='THIS IS MY SECOND BUTTON IN REACT NATIVE'></Button>
-      <Text style = {{fontSize:30}}> Hello THIRD Text Components</Text>
-      <Button title='PRESS ME'></Button>
+      <Text  style = {{fontSize:30}}>Email Id: {email}</Text>
 
-    
+      <Text  style = {{fontSize:30}}>Fruit :{fruit()}</Text>
+
+      <Text  style = {{fontSize:30}}>{fruit()}</Text>
+      <Button title='Submit Button'></Button>
+      <UserData/>
+      <UserData/>
+      
+    </View>
+  )
+}
+
+const UserData = ()=>{
+  return(
+    <View>
+
+<Text  style = {{fontSize:30}}>Name : {name}</Text>
+
+<Text  style = {{fontSize:30}}>Age : {age}</Text>
+
+<Text  style = {{fontSize:30}}>Email Id: {email}</Text>
+
+<Text  style = {{fontSize:30}}>Fruit :{fruit()}</Text>
+
+<Text  style = {{fontSize:30}}>{fruit()}</Text>
+<Button title='Submit Button'></Button>
     </View>
   )
 }
