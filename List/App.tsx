006f9bb13fa79e9a1f5752ec1a6ab69836cd34ee
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-10-16 19:43:58 +0530

    List

---
+++ List/App.tsx
@@ -0,0 +1,36 @@
+import type {PropsWithChildren} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = ()=>{
+  const users = [
+    {
+      id:1,
+      name:"Sagar"
+    },
+    {
+      id:2,
+      name:"Sam"
+    }
+    ,
+    {
+      id:3,
+      name:"Peter"
+    }
+    ,
+    {
+      id:4,
+      name:"Tony"
+    }
+  ]
+
+    return(
+      <View>
+
+      </View>
+    )
+}
+export default App; 
